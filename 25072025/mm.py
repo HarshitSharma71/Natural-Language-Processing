006460f0becf43e61f38857feea96a95c7c0201c
HarshitSharma71 <122046924+HarshitSharma71@users.noreply.github.com>
2025-07-25 17:02:56 +0530

    c22

--- 25072025/mm.py
+++ 25072025/mm.py
@@ -1,4 +1,12 @@
-a= int(input("Enter the first number"))
-b= int(input("Enter the second number"))
-sum = a+b
-print (sum)
+# a= int(input("Enter the first number"))
+# b= int(input("Enter the second number"))
+# sum = a+b
+# print (sum)
+# wap to calculate factorial of a number
+n = int(input("enter the number"))
+
+fact = 1
+for i in range(1, n + 1):
+    fact *= i
+
+print(fact)
