7f082461e88e09f88b6dc210cc7b1e386d633813
HarshitSharma71 <122046924+HarshitSharma71@users.noreply.github.com>
2025-07-25 17:15:51 +0530

    cc222

--- 25072025/mm.py
+++ 25072025/mm.py
@@ -3,10 +3,17 @@
 # sum = a+b
 # print (sum)
 # wap to calculate factorial of a number
-n = int(input("enter the number"))
+# n = int(input("enter the number"))
 
-fact = 1
-for i in range(1, n + 1):
-    fact *= i
+# fact = 1
+# for i in range(1, n + 1):
+#     fact *= i
 
-print(fact)
+# print(fact)
+
+
+#wap to calculate area of rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+print(f"The area of the rectangle is: {area} square units.")
